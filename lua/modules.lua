f4b01641998bb10137061476ee364adcc6c9be30
han8129 <hoang8129@gmail.com>
2023-02-22 23:53:52 +0700

    add fail save for coc

--- lua/modules.lua
+++ lua/modules.lua
@@ -18,3 +18,6 @@ return require('packer').startup(function(use)
 	use {'neoclide/coc.nvim', branch = 'release'}
 
 end)
+
+vim.g.coc_global_extensions = { 'coc-java' ,'coc-html' ,'coc-css' ,'coc-cssmodules' ,'coc-emmet' ,'coc-highlight' ,'coc-stylelintplus' ,'coc-prettier' ,'coc-markdownlint' ,'@yaegassy/coc-tailwindcss3' ,'coc-json' ,'coc-tabnine' ,'coc-pyright' }
+keymap("n", "<leader>coc", ":CocCommand ", options)
