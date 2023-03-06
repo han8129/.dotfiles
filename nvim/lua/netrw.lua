507d0663a654cdde4e7b13c3fd068a7e49f420cf
han8129 <110092300+han8129@users.noreply.github.com>
2023-03-06 21:53:03 +0700

    change modifier to Window and more nvim config

--- nvim/lua/netrw.lua
+++ nvim/lua/netrw.lua
@@ -1,4 +1,6 @@
 vim.cmd([[
+let g:netrw_list_hide = '\(^\|\s\s\)\zs\.\S\+'
+
 if &columns < 100
 	" If the screen is small, occupy half
 	let g:netrw_winsize = 50
@@ -6,6 +8,19 @@ else
 	" else take 30%
 	let g:netrw_winsize = 25
 	endif
+
+function! NetrwMapping()
+	nmap <buffer> h -^
+	nmap <buffer> l <CR>
+	nmap <buffer> . gh
+	nmap <buffer> L <CR>:Lexplore<CR>
+	nmap <buffer> P <C-w>z
+endfunction
+
+augroup netrw_mapping
+	autocmd!
+	autocmd filetype netrw call NetrwMapping()
+augroup END
 ]])
 
 local netrwOptions = {
@@ -15,6 +30,7 @@ local netrwOptions = {
 	netrw_preview = 1,
 	netrw_altv = 1,
 	netrw_keepdir = 0,
+	netrw_localcopydircmd = 'cp -r',
 }
 
 
